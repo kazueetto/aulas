88ba9bdfce2a18d612f4923f8deeb66ad7c2b44c
kazueetto <48527371+kazueetto@users.noreply.github.com>
2019-06-12 13:50:48 -0300

    Update game.py

--- jogo/game.py
+++ jogo/game.py
@@ -7,10 +7,10 @@ POS = (x,y)
 SPRITE = "bolota.png"
 
 import pygame
-from serial import Serial
+import serial
 import time
  
-ser = serial.Serial('/dev/ttyACM0', 9600)
+ser = serial.Serial('COM8', 9600, timeout=0)
 
 pygame.init()
 
@@ -23,30 +23,36 @@ sprite = pygame.image.load (SPRITE)
 
 while LOOP:
     porta =ser.read()
+    decodificado=porta.decode('utf-8')
     time.sleep(0.2)
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             LOOP = False
     screen.blit (sprite, POS)
    
-    if porta == "E":
+    if decodificado == "E":
         x=x-10
         POS = (x,y)
         pygame.display.update()
         
 
-    if porta == "D":
+    if decodificado == "D":
         x=x+10
         POS = (x,y)
         pygame.display.update()
 
 
-    if porta == "C":
+    if decodificado == "C":
         y=y+10
         POS = (x,y)
         pygame.display.update()
 
-    if porta == "B":
+    if decodificado == "B":
+        #print("teste1")
         y=y-10
         POS = (x,y)
         pygame.display.update()
+        #print("teste2")
+        
+        
+    print(decodificado)
