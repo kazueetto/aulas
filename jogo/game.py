b75e45c743966849e59210f23bb35b2c884bfcb3
kazueetto <48527371+kazueetto@users.noreply.github.com>
2019-06-12 15:05:28 -0300

    Update game.py

--- jogo/game.py
+++ jogo/game.py
@@ -9,6 +9,8 @@ SPRITE = "BATEMAN.png"
 import pygame
 import serial
 import time
+
+CIDADE = pygame.image.load("cidade.jpg")
  
 ser = serial.Serial('COM8', 9600, timeout=0)
 
@@ -18,8 +20,7 @@ pygame.init()
 screen = pygame.display.set_mode(RESOL)
 screen.fill (BLACK)
 sprite = pygame.image.load (SPRITE)
-
-
+screen.blit (CIDADE, (0,0))
 
 while LOOP:
     porta =ser.read()
@@ -30,29 +31,33 @@ while LOOP:
             LOOP = False
     screen.blit (sprite, POS)
    
-    if decodificado == "E":
+    if (decodificado == "E" and x>0):
         x=x-10
         POS = (x,y)
-        pygame.display.update()
+        pygame.display.flip()
         
 
-    if decodificado == "D":
+    if (decodificado == "D" and x<800):
         x=x+10
         POS = (x,y)
         pygame.display.update()
 
 
-    if decodificado == "C":
+    if (decodificado == "C" and y<600):
         y=y+10
         POS = (x,y)
         pygame.display.update()
 
-    if decodificado == "B":
+    if (decodificado == "B" and y>0):
         #print("teste1")
         y=y-10
         POS = (x,y)
         pygame.display.update()
         #print("teste2")
         
+    if decodificado == "P":
+        pygame.mixer.music.load('musica.mp3')
+        pygame.mixer.music.play()
+        pygame.event.wait()
         
     print(decodificado)
